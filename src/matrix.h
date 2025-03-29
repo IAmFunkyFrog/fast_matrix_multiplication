6144fff41b2500d3cb3cb9e0cd7b57aceb3dc3b9
Stepan Trefilov <trefilov.stepan@gmail.com>
2025-03-29 13:36:59 +0300

    Remove LOWER_TRIANGULAR_COLS matrix type and add matrix_type_info_t for blocked types

--- src/matrix.h
+++ src/matrix.h
@@ -9,11 +9,17 @@
 typedef enum {
     NORMAL,
     UPPER_TRIANGULAR_COLS,
-    LOWER_TRIANGULAR_COLS
+    NORMAL_BLOCKED,
+    UPPER_TRIANGULAR_BLOCKED
 } matrix_type_t;
+
+typedef union {
+    int block_size; // for NORMAL_BLOCKED and UPPER_TRIANGULAR_BLOCKED
+} matrix_type_info_t;
 
 typedef struct {
     matrix_type_t type;
+    matrix_type_info_t minfo;
     int ncols;
     int nrows;
     void *data;
@@ -24,8 +30,7 @@ static inline int matrix_index_in_matrix(double_matrix_t matrix, int i, int j) {
     if (j >= matrix.ncols || j < 0) return 0;
 
     if (matrix.type == NORMAL) return 1;
-    if (matrix.type == UPPER_TRIANGULAR_COLS && i <= j) return 1;
-    if (matrix.type == LOWER_TRIANGULAR_COLS && i >= j) return 1;
+    if ((matrix.type == UPPER_TRIANGULAR_COLS || matrix.type == UPPER_TRIANGULAR_BLOCKED) && i <= j) return 1;
 
     return 0;
 }
@@ -60,8 +65,6 @@ static inline double matrix_get(double_matrix_t matrix, int i, int j) {
         return plain[i * matrix.ncols + j];
     case UPPER_TRIANGULAR_COLS:
         return plain[j * (j + 1) / 2 + i];
-    case LOWER_TRIANGULAR_COLS:
-        return plain[j * (2 * matrix.nrows - j + 1) / 2 + i];
     default:
         assert(0 && "unsupported");
     }
@@ -79,9 +82,6 @@ static inline void matrix_set(double_matrix_t matrix, int i, int j, double value
         return;
     case UPPER_TRIANGULAR_COLS:
         plain[j * (j + 1) / 2 + i] = value;
-        return;
-    case LOWER_TRIANGULAR_COLS:
-        plain[j * (2 * matrix.nrows - j + 1) / 2 + i];
         return;
     default:
         assert(0 && "unsupported");
@@ -102,16 +102,6 @@ static inline double_matrix_t matrix_allocate_upper_triangular_cols(int nrows) {
     assert(nrows > 0);
     return (double_matrix_t) {
         .type = UPPER_TRIANGULAR_COLS,
-        .ncols = nrows,
-        .nrows = nrows,
-        .data = calloc(1, (sizeof(double) / 2) * (1 + nrows) * nrows)
-    };
-}
-
-static inline double_matrix_t matrix_allocate_lower_triangular_cols(int nrows) {
-    assert(nrows > 0);
-    return (double_matrix_t) {
-        .type = LOWER_TRIANGULAR_COLS,
         .ncols = nrows,
         .nrows = nrows,
         .data = calloc(1, (sizeof(double) / 2) * (1 + nrows) * nrows)
@@ -144,13 +134,6 @@ static double_matrix_t matrix_convert_to_upper_triangular_cols(double_matrix_t m
     return out;
 }
 
-static double_matrix_t matrix_convert_to_lower_triangular_cols(double_matrix_t m) {
-    assert(m.nrows == m.ncols && "only square matrices supported");
-    double_matrix_t out = matrix_allocate_lower_triangular_cols(m.nrows);
-    matrix_convert(m, out);
-    return out;
-}
-
 void matrix_fill_random(double_matrix_t matrix);
 
 void matrix_mult3(double_matrix_t m1, double_matrix_t m2, double_matrix_t out);
@@ -167,42 +150,4 @@ static inline double_matrix_t matrix_mult_block2(double_matrix_t m1, double_matr
     double_matrix_t out = matrix_allocate(m1.nrows, m2.ncols);
     matrix_mult_block3(m1, m2, out, block_max_size);
     return out;
-}
-
-static inline size_t matrix_serialized_size(double_matrix_t m) {
-    return sizeof(double_matrix_t) + sizeof(double) * m.ncols * m.nrows;
-}
-
-static inline void matrix_serialize_to_buffer_inplace(void *buf, double_matrix_t m) {
-    int *ncols = (int *) buf;
-    int *nrows = (int *) (buf + sizeof(int));
-    void *data = buf + 2 * sizeof(int);
-
-    *ncols = m.ncols;
-    *nrows = m.nrows;
-    memcpy(data, m.data, m.ncols * m.nrows * sizeof(double));
-}
-
-static inline void *matrix_serialize_to_buffer(double_matrix_t m) {
-    size_t size = matrix_serialized_size(m);
-    void *buf = malloc(size);
-    matrix_serialize_to_buffer_inplace(buf, m);
-    return buf;
-}
-
-static inline void matrix_deserialize_from_buffer_inplace(void *buf, double_matrix_t *m) {
-    int *ncols = (int *) buf;
-    int *nrows = (int *) (buf + sizeof(int));
-    void *data = buf + 2 * sizeof(int);
-
-    m->ncols = *ncols;
-    m->nrows = *nrows;
-    m->data = malloc(m->ncols * m->nrows * sizeof(double));
-    memcpy(m->data, data, m->ncols * m->nrows * sizeof(double));
-}
-
-static inline double_matrix_t matrix_deserialize_from_buffer(void *buf) {
-    double_matrix_t m;
-    matrix_deserialize_from_buffer_inplace(buf, &m);
-    return m;
 }
